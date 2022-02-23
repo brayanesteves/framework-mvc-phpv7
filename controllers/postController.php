47186d97a88eab49ade4416902ad80dc8f273406
Brayan Esteves <werneriasearch@gmail.com>
2022-02-22 22:11:46 -0400

    ✔️Views [Posts]✔️ - Bootstrap (Version 5.1.3)

--- controllers/postController.php
+++ controllers/postController.php
@@ -6,7 +6,13 @@
             $this->_post = $this->loadModel('post');
         }
 
-        public function index() {            
+        public function index() {   
+            // Load First
+            $this->_view->setLibsCSSJS(array('bootstrap/version/5.1.3/dist/css/bootstrap.min'), 0);
+            $this->_view->setLibsJS(array('angular.js/version/1.8.2/angular.min', 'jquery/version/3.6.0/js/jquery-3.6.0.min'));
+            // Load Second
+            $this->_view->setJS(array('main'));
+                     
             $this->_view->posts = $this->_post->getPosts();
             $this->_view->title = 'Posts';
             $this->_view->render('index', 'post');
